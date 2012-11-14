ce000917c48b17fe5235c0141a1ef97f94fb4ac5
Ed Barnard <ed@edwardbarnard.com>
2012-11-14 09:07:29 +0000

    Comment code and convert tabs to spaces.

--- udp_obfs.c
+++ udp_obfs.c
@@ -1,3 +1,5 @@
+// ORIGINAL EDITED
+
 // gcc -o udp_obfs udp_obfs.c
 
 /* http://www.brokestream.com/udp_redirect.html
@@ -35,52 +37,68 @@
 #include <netinet/in.h>
 
 int main(int argc, char *argv[]) {
-	if (argc!=3 && argc!=5) {
-		printf("Usage: %s our-ip our-port send-to-ip send-to-port\n",argv[0]);
-		printf("Usage: %s our-ip our-port             # echo mode\n",argv[0]);
-		exit(1);
-	}
+    if (argc! = 3 && argc! = 5) {
+        printf("Usage: %s our-ip our-port send-to-ip send-to-port\n", argv[0]);
+        printf("Usage: %s our-ip our-port             # echo mode\n", argv[0]);
+        exit(1);
+    }
     
     int key_length = 64;
     //char key[key_length];
+    // Should really be 65535 bytes...
     char* key = "1234567890123456789012345678901234567890123456789012345678901234";
 
-	int os=socket(PF_INET,SOCK_DGRAM,IPPROTO_IP);
+    // Init socket
+    int os = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
 
-	struct sockaddr_in a;
-	a.sin_family=AF_INET;
-	a.sin_addr.s_addr=inet_addr(argv[1]); a.sin_port=htons(atoi(argv[2]));
-	if(bind(os,(struct sockaddr *)&a,sizeof(a)) == -1) {
-		printf("Can't bind our address (%s:%s)\n", argv[1], argv[2]);
-		exit(1); }
+    struct sockaddr_in a;
+    a.sin_family = AF_INET;
+    a.sin_addr.s_addr = inet_addr(argv[1]);
+    a.sin_port = htons(atoi(argv[2]));
+    
+    // Try to bind to local port
+    if(bind(os, (struct sockaddr *)&a, sizeof(a)) == -1) {
+        printf("Can't bind our address (%s:%s)\n", argv[1], argv[2]);
+        exit(1);
+    }
 
-	if(argc==5) { a.sin_addr.s_addr=inet_addr(argv[3]); a.sin_port=htons(atoi(argv[4])); }
+    // TODO: Remove the echo service - although it could be useful for internal application/scripting use
+    if(argc == 5) {
+        a.sin_addr.s_addr = inet_addr(argv[3]);
+        a.sin_port = htons(atoi(argv[4]));
+    }
 
-	struct sockaddr_in sa;
-	struct sockaddr_in da; da.sin_addr.s_addr=0;
-	while(1) {
-		char buf[65536]; // Better than 65535 for 64bit xor key
-		int sn=sizeof(sa);
-		int n=recvfrom(os,buf,sizeof(buf),0,(struct sockaddr *)&sa,&sn);
-		if(n<=0) continue;
+    struct sockaddr_in sa; // The source address of the incoming packet
+    struct sockaddr_in da; // The address of the connection initiator
+    da.sin_addr.s_addr = 0;
+    
+    while(1) {
+        char buf[65535];
+        int sn = sizeof(sa);
+        int n = recvfrom(os, buf, sizeof(buf), 0, (struct sockaddr *)&sa, &sn);
+        if(n <= 0) continue;
 
         int i;
         for(i = 0; i < n; i++)
         {
-            // Encrypt/decrypt in place
+            // Encrypt/decrypt (XOR) in place - no buffers
             buf[i] = buf[i] ^ key[i%key_length];
         }
 
-		if(argc==3) {
+        if(argc == 3) {
             // Echo mode
-            sendto(os,buf,n,0,(struct sockaddr *)&sa,sn);
-		} else if(sa.sin_addr.s_addr==a.sin_addr.s_addr && sa.sin_port==a.sin_port) {
-            // Send to destination
-			if(da.sin_addr.s_addr) sendto(os,buf,n,0,(struct sockaddr *)&da,sizeof(da));
-		} else {
-            // Send to source
-			sendto(os,buf,n,0,(struct sockaddr *)&a,sizeof(a));
-			da=sa;
-		}
-	}
+            sendto(os, buf, n, 0, (struct sockaddr *)&sa, sn);
+        } else if(sa.sin_addr.s_addr == a.sin_addr.s_addr && sa.sin_port == a.sin_port) { // Packet coming from destination
+            // Check if we actually know what the incoming/local address is
+            if(da.sin_addr.s_addr) {
+                // Send packet to connection initiator
+                sendto(os, buf, n, 0, (struct sockaddr *)&da, sizeof(da));
+            }
+        } else { // Packet coming from source
+            // Send packet to destination port
+            sendto(os, buf, n, 0, (struct sockaddr *)&a, sizeof(a));
+            // Save address of connection initiator
+            da = sa;
+        }
+    }
 }
